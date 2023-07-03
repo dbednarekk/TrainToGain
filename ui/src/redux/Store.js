7ee07622ada279f21cd93c097bac70db5bd75be8
Damian Bednarek <damian.bednarekk@gmail.com>
2023-07-03 15:27:42 +0200

    removed frontend to start from scratch

--- ui/src/redux/Store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import ExerciseSliceReducer from "./ExerciseSlice";
-import UserSliceReducer from "./UserSlice";
-export default configureStore({
-  reducer: {
-    userSlices: UserSliceReducer,
-    exerciseSlices: ExerciseSliceReducer
-  },
-});
--- ui/src/services/Regex.js
+++
@@ -1,6 +0,0 @@
-import XRegExp from "xregexp"
-export const LOGIN_REGEX = XRegExp("^\\w{3,30}$")
-export const PASSWORD_REGEX = XRegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*\\W).{8,}$")
-export const NAME_REGEX = XRegExp("^[\\p{L}' -]+\\.?$")
-export const EMAIL_REGEX = XRegExp("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])")
-export const NUMBER_REGEX = XRegExp("^\\+?(0|[1-9]\\d*)$")
